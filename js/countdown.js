62cbe20ad396ce0b37189d9c436e78a1fe45e593
Ruben Schwarz <schwarzruben@gmail.com>
2015-12-19 13:14:50 +0100

    add countdown

---
+++ js/countdown.js
@@ -0,0 +1,25 @@
+
+
+var endDate = new Date(2016, 8, 24, 13, 30, 00);
+
+function countdown () {
+
+    var actDate = new Date();
+
+    var diff = endDate - actDate;
+
+
+    var diffSeconds = Math.floor(diff / 1000);
+    var diffMin = Math.floor(diffSeconds / 60);
+    var diffHour = Math.floor(diffMin / 60);
+    var diffDay = Math.floor(diffHour / 24);
+
+    //console.log(endDate, actDate, diffDay, diffHour, diffMin, diffSeconds);
+
+    document.getElementById("cd-day").innerHTML = diffDay;
+    document.getElementById("cd-hour").innerHTML = diffHour % 24;
+    document.getElementById("cd-min").innerHTML = diffMin % 60;
+    document.getElementById("cd-sec").innerHTML = diffSeconds % 60;
+
+    setTimeout('countdown()',200);
+}
