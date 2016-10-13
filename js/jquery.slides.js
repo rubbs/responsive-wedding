22775daedc1fdcf4b1087aa4cb5ec2f338825ef9
ruben <schwarzruben@gmail.com>
2016-10-13 22:37:15 +0200

    images

--- js/jquery.slides.js
+++ js/jquery.slides.js
@@ -82,7 +82,7 @@
                 position: "absolute",
                 top: 0,
                 left: 0,
-                width: "100%",
+                height: "100%",
                 zIndex: 0,
                 display: "none",
                 webkitBackfaceVisibility: "hidden"
